9d17f403f7c8f288c1d2200ccb7879ca9f18e944
SHP-ART <SHP-ART@users.noreply.github.com>
2026-01-01 23:58:34 +0100

    Clean up repository structure

--- backend/src/routes/auslastungRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const TermineController = require('../controllers/termineController');
-
-router.get('/:datum', TermineController.getAuslastung);
-
-module.exports = router;
